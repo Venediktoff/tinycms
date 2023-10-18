e1de73bce1e4fa07753c8cffa59d46b8e0838ace
Ivan Venediktov <ivan.venediktov@hurtigruten.com>
2023-10-18 09:10:32 +0100

    Clear cache before buiding

--- converter.js
+++ converter.js
@@ -17,7 +17,41 @@ function getAllFiles(dirPath, arrayOfFiles) {
   return arrayOfFiles;
 }
 
+function getAllJsonFiles(dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(file => {
+    const fullPath = path.join(dirPath, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      arrayOfFiles = getAllJsonFiles(fullPath, arrayOfFiles);
+    } else if (file.endsWith('.json')) {
+      arrayOfFiles.push(fullPath);
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+function clearCache(dirPath) {
+    const files = fs.readdirSync(dirPath);
+  
+    files.forEach(file => {
+      const fullPath = path.join(dirPath, file);
+      if (fs.statSync(fullPath).isDirectory()) {
+        clearCache(fullPath);
+      } else if (file.endsWith('.json')) {
+        fs.unlinkSync(fullPath);
+      }
+    });
+  }
+  
+
 const dataDir = path.resolve(__dirname, 'data');
+const cacheDir = path.resolve(__dirname, 'cache');
+
+clearCache(cacheDir);
 const files = getAllFiles(dataDir);
 
 files.forEach(file => {
@@ -33,10 +67,35 @@ files.forEach(file => {
     obj[tag] = text;
   }
 
-  const cacheDir = path.resolve(__dirname, 'cache');
+
   const relativePath = path.relative(dataDir, file);
   const jsonPath = path.join(cacheDir, relativePath.replace(/\.tml$/, '.json'));
 
+
   fs.mkdirSync(path.dirname(jsonPath), { recursive: true });
   fs.writeFileSync(jsonPath, JSON.stringify(obj, null, 2), 'utf8');
+});
+
+// const cacheDir = path.resolve(__dirname, 'cache');
+const jsonFiles = getAllJsonFiles(cacheDir);
+
+jsonFiles.forEach(file => {
+  const json = fs.readFileSync(file, 'utf8');
+  const obj = JSON.parse(json);
+
+  const dirPath = path.dirname(file);
+  const allJsonPath = path.join(dirPath, '_all.json');
+
+  let allData;
+  try {
+    const allJson = fs.readFileSync(allJsonPath, 'utf8');
+    allData = JSON.parse(allJson);
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+    allData = [];
+  }
+
+  allData.push(obj);
+
+  fs.writeFileSync(allJsonPath, JSON.stringify(allData, null, 2), 'utf8');
 });
--- server.js
+++ server.js
@@ -7,22 +7,48 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 const returnCache = (location, res) => {
-   fs.readFile('./cache'+location+'.json', 'utf8', (err, data) => {
-        if (err) {
-          return;
-        }
 
-        const response = JSON.parse(data);
-        res.status(200).json(response)
-    })
+const path = './cache'+location;
+console.log(path+'.json')
+
+if (fs.existsSync(path)) {
+    const stats = fs.statSync(path);
+
+    if (stats.isDirectory()) {
+        fs.readFile(path+'/_all.json', 'utf8', (err, data) => {
+            if (err) {
+              return;
+            }
+    
+            const response = JSON.parse(data);
+            res.status(200).json(response)
+        })
+    } else {
+        fs.readFile(path+'.json', 'utf8', (err, data) => {
+            if (err) {
+              return;
+            }
+    
+            const response = JSON.parse(data);
+            res.status(200).json(response)
+        })
+    }
+    
+    } else {
+        console.log('The file or directory does not exist');
+        res.status(200).json({});
+
+    }
+
+   
 }
 
-routes.map(r => {
-app.route(r.route)
+
+app.route('*')
     .get((req, res) => {
-       returnCache(r.route, res)
+        console.log(req.originalUrl)
+       returnCache(req.originalUrl, res)
     })
-})
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
